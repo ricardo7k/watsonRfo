d54ddc5a6cc3e67074da1b65dac2fe600aebb897
Caca Ferreira <ricardo7k@yahoo.com.br>
2017-03-24 19:07:16 -0300

    link com translate

--- watson.js
+++ watson.js
@@ -1,14 +1,29 @@
 'use strict';
 
+const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+const LanguageTranslatorV2 = require('watson-developer-cloud/language-translator/v2');
 const HttpDispatcher = require('httpdispatcher');
 const dispatcher     = new HttpDispatcher();
 const http = require('http')
 const fs = require('fs');
-const NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+
+var cors = require('cors');
+var express = require('express');
+var querystring = require('querystring');
+var app = express();
+app.use(cors());
+
+var language_translator = new LanguageTranslatorV2(
+{
+  username: '',
+  password: '',
+  url: 'https://gateway.watsonplatform.net/language-translator/api/'
+}
+);
 
 var nlu = new NaturalLanguageUnderstandingV1({
-  username: " ",
-  password: " ",
+  username: "c21bfce9-6722-4da2-b449-a024a74a4087",
+  password: "ZXOQcXYWBWZe",
   version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
 });
 
@@ -20,7 +35,7 @@ function handleRequest(request, response){
   }
 }
 
-dispatcher.onGet("/",function(req,res){
+dispatcher.onGet("/",function(req, res){
   fs.readFile('index.html', function (err, html) {
     res.writeHeader(200, {"Content-Type": "text/html"});
     res.write(html);
@@ -28,7 +43,7 @@ dispatcher.onGet("/",function(req,res){
   });
 });
 
-dispatcher.onPost("/send",function(req,res){
+dispatcher.onPost("/send",function(req, res){
   nlu.analyze({
     'html': req.params.text,
     'features': {
@@ -42,8 +57,33 @@ dispatcher.onPost("/send",function(req,res){
     var content = "";
     if (err) content = err;
     else content = JSON.stringify(response, null, 2);
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end(content);
+    var arr = [
+      response.sentiment.document.label,
+      response.categories[0].label.substring(1,response.categories[0].label.length).split("/").join(", ")
+    ];
+
+    var options = {
+      host: 'localhost',
+      port: 8083,
+      path: '/translate',
+      method: 'POST'
+    };
+
+    var httpreq = http.request(options, function (response) {
+      response.setEncoding('utf8');
+      response.on('data', function (chunk) {
+        chunk = JSON.parse(chunk);
+        var content = chunk.translations[0].translation.split(";");
+        var frase = "Estou aprendendo com você, aparentemente você está falando algo " + content[0] + " sobre " + content[1] + "é isto?";
+        res.writeHeader(200, {"Content-Type": "text/plain"});
+        res.write(frase);
+        res.end();
+      });
+    });
+    httpreq.write(querystring.stringify({
+      text: arr[0] + ";" + arr[1]
+    }));
+    httpreq.end();
   });
 });
 
